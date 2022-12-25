d41c6141652dc24d67c96230d7abd28970312969
vijay@envivo <vijay.patel@envivo.co.uk>
2022-12-25 12:07:42 +0000

    feat: Added example of Value Object

---
+++ Fresnel.Sample.Features.Model/A_Objects/Basics/ValueObject.cs
@@ -0,0 +1,37 @@
+// SPDX-FileCopyrightText: Copyright (c) 2022 Envivo Software
+// SPDX-License-Identifier: Apache-2.0
+using Envivo.Fresnel.ModelTypes.Interfaces;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Envivo.Fresnel.Sample.Features.Model.A_Objects.Basics
+{
+    /// <summary>
+    /// This object is only editable when new, and becomes read-only once it's saved.
+    /// </summary>
+    public class ValueObject : IValueObject, IPersistable
+    {
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        [Key]
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        [ConcurrencyCheck]
+        public long Version { get; set; }
+
+        /// <summary>
+        /// The person's name
+        /// </summary>
+        public string FullName { get; set; }
+
+        /// <summary>
+        /// The person's date of birth
+        /// </summary>
+        [DataType(DataType.Date)]
+        public DateTime DateOfBirth { get; set; }
+    }
+}
