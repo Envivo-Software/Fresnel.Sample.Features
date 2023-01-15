f46ffce6fc9a0fca5a861ea244c83ce9c11af7bc
vijay@envivo <vijay.patel@envivo.co.uk>
2023-01-15 09:21:02 +0000

    feat: Added example of ObjectWithInjectedDependencies

---
+++ Fresnel.Sample.Features.Model/J_Services/ObjectWithInjectedDependencies.cs
@@ -0,0 +1,29 @@
+// SPDX-FileCopyrightText: Copyright (c) 2022 Envivo Software
+// SPDX-License-Identifier: Apache-2.0
+using Envivo.Fresnel.ModelTypes.Interfaces;
+using System;
+
+namespace Envivo.Fresnel.Sample.Features.Model.J_Services
+{
+    /// <summary>
+    /// This Domain Services provide access to logic without instantiating an object
+    /// </summary>
+    public class ObjectWithInjectedDependencies : IEntity
+    {
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// This service is injected, and will not appear in the UI
+        /// </summary>
+        public DomainService DomainService { get; set; }
+
+        /// <summary>
+        /// Invokes a method using the DomainService
+        /// </summary>
+        /// <returns></returns>
+        public string InvokeService()
+        {
+            return DomainService?.DownloadFile();
+        }
+    }
+}
