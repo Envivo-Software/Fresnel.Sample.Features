de7e968979be8d73c91a2e3cc5dc73b5c5691f48
vijay@envivo <vijay.patel@envivo.co.uk>
2023-03-08 09:00:32 +0000

    feat: Added examples of Properties that have inheritance

--- Fresnel.Sample.Features.Model/A_Objects/InheritanceByInterface/ObjectC.cs
+++ Fresnel.Sample.Features.Model/A_Objects/InheritanceByInterface/ObjectC.cs
@@ -1,6 +1,5 @@
 ﻿// SPDX-FileCopyrightText: Copyright (c) 2022-2023 Envivo Software
 // SPDX-License-Identifier: Apache-2.0
-using Envivo.Fresnel.ModelAttributes;
 using System;
 
 namespace Envivo.Fresnel.Sample.Features.Model.A_Objects.InheritanceByInterface
@@ -13,9 +12,6 @@ namespace Envivo.Fresnel.Sample.Features.Model.A_Objects.InheritanceByInterface
         public Guid Id { get; set; }
 
         public string A_StringValue { get; set; }
-
-        [Relationship(RelationshipType.Owns)]
-        public IBaseObject NestedObject { get; set; }
 
         public void A_Method()
         {
