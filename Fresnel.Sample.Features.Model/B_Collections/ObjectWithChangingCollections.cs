77ddc7aa88f7ffa9d925a7524fe8c79a1ba2c7e9
vijay@envivo <vijay.patel@envivo.co.uk>
2023-09-22 10:06:20 +0100

    feat: Added ObjectWithChangingCollections, so show collections changes made by internal code

---
+++ Fresnel.Sample.Features.Model/B_Collections/ObjectWithChangingCollections.cs
@@ -0,0 +1,73 @@
+// SPDX-FileCopyrightText: Copyright (c) 2022-2023 Envivo Software
+// SPDX-License-Identifier: Apache-2.0
+using Envivo.Fresnel.Sample.Features.Model.A_Objects.Basics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Envivo.Fresnel.Sample.Features.Model.B_Collections
+{
+    /// <summary>
+    /// This object has collections with other objects within them
+    /// </summary>
+    public class ObjectWithChangingCollections
+    {
+        private ICollection<BasicObject> _PreDefinedItems;
+
+        /// <summary>
+        /// The unique Id for this entity
+        /// </summary>
+        public Guid Id { get; set; }
+
+        /// <summary>
+        ///
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// This collection's contents change every time the button is clicked
+        /// </summary>
+        public List<BasicObject> Items { get; internal set; } = new List<BasicObject>();
+
+        public int ItemsPerPage { get; set; } = 10;
+
+        /// <summary>
+        /// Changes the associated Collection's contents
+        /// </summary>
+        public void ChangeContents()
+        {
+            _PreDefinedItems ??= CreatePreDefinedItems();
+
+            var replacementSet =
+                _PreDefinedItems
+                .OrderBy(i => Guid.NewGuid())
+                .Take(ItemsPerPage)
+                .ToList();
+            Items.Clear();
+            Items.AddRange(replacementSet);
+        }
+
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return Name;
+        }
+
+        private ICollection<BasicObject> CreatePreDefinedItems()
+        {
+            return
+                Enumerable.Range(1, ItemsPerPage * 4)
+                .Select(i => new BasicObject
+                {
+                    Id = Guid.NewGuid(),
+                    Name = $"Item No{i}",
+                    Description = $"This is the description for item number {i}"
+                })
+                .ToList();
+        }
+
+    }
+}
