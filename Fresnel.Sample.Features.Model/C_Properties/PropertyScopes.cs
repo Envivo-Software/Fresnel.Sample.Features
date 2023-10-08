698d217619636c5d052872bb511ce7dfbced5291
vijay@envivo <vijay.patel@envivo.co.uk>
2023-10-08 17:48:34 +0100

    feat: Added examples of Property scopes

---
+++ Fresnel.Sample.Features.Model/C_Properties/PropertyScopes.cs
@@ -0,0 +1,23 @@
+// SPDX-FileCopyrightText: Copyright (c) 2022-2023 Envivo Software
+// SPDX-License-Identifier: Apache-2.0
+using System;
+
+namespace Envivo.Fresnel.Sample.Features.Model.C_Properties
+{
+    public class PropertyScopes
+    {
+        public Guid Id { get; set; }
+
+        public string PublicGetterAndPublicSetter { get; set; }
+
+        public string PublicGetterAndInternalSetter { get; internal set; }
+
+        public string PublicGetterAndPrivateSetter { get; private set; }
+
+        internal string InternalGetterAndInternalSetter { get; set; }
+
+        public string PrivateGetterAndPublicSetter { private get; set; }
+
+        private string PrivateGetterAndPrivateSetter { get; set; }
+    }
+}
