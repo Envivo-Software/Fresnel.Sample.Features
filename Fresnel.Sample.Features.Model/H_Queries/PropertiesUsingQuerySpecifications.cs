027a8b25639665cec81fc94e54cb1062547022f7
vijay@envivo <vijay.patel@envivo.co.uk>
2023-10-08 17:51:03 +0100

    feat: Added example of Property QuerySpecifications

---
+++ Fresnel.Sample.Features.Model/H_Queries/PropertiesUsingQuerySpecifications.cs
@@ -0,0 +1,70 @@
+// SPDX-FileCopyrightText: Copyright (c) 2022-2023 Envivo Software
+// SPDX-License-Identifier: Apache-2.0
+using Envivo.Fresnel.ModelAttributes;
+using Envivo.Fresnel.ModelTypes;
+using Envivo.Fresnel.ModelTypes.Interfaces;
+using Envivo.Fresnel.Sample.Features.Model.A_Objects.Basics;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Envivo.Fresnel.Sample.Features.Model.H_Queries
+{
+    /// <summary>
+    /// This object shows how QuerySpecifications are used on properties
+    /// </summary>
+    public class PropertiesUsingQuerySpecifications : IEntity
+    {
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        [Key]
+        public Guid Id { get; set; }
+
+/// <summary>
+/// This object will open a selection dialog
+/// </summary>
+[Relationship(RelationshipType.Has)]
+[FilterQuerySpecification(typeof(SaveableEntityQuerySpecification))]
+public SaveableEntity SingleEntityUsingDialog { get; set; }
+
+        /// <summary>
+        /// This object will open a selection list
+        /// </summary>
+        [Relationship(RelationshipType.Has)]
+        [FilterQuerySpecification(typeof(SaveableEntityQuerySpecification))]
+        [UI(preferredControl: UiControlType.Select)]
+        public SaveableEntity SingleEntityUsingSelectList { get; set; }
+
+        /// <summary>
+        /// This collection will open a selection dialog
+        /// </summary>
+        [Relationship(RelationshipType.Has)]
+        [FilterQuerySpecification(typeof(SaveableEntityQuerySpecification))]
+        public ICollection<SaveableEntity> MultipleEntitiesUsingDialog { get; set; } = new List<SaveableEntity>();
+
+
+
+        /// <summary>
+        /// This object will open a selection dialog
+        /// </summary>
+        [Relationship(RelationshipType.Has)]
+        [FilterQuerySpecification(typeof(SaveableEntityQuerySpecification))]
+        public AggregateReference<SaveableEntity> SingleAggregateRefUsingDialog { get; set; }
+
+        /// <summary>
+        /// This object will open a selection list
+        /// </summary>
+        [Relationship(RelationshipType.Has)]
+        [FilterQuerySpecification(typeof(SaveableEntityQuerySpecification))]
+        [UI(preferredControl: UiControlType.Select)]
+        public AggregateReference<SaveableEntity> SingleAggregateRefUsingSelectList { get; set; }
+
+        /// <summary>
+        /// This collection will open a selection dialog
+        /// </summary>
+        [Relationship(RelationshipType.Has)]
+        [FilterQuerySpecification(typeof(SaveableEntityQuerySpecification))]
+        public ICollection<AggregateReference<SaveableEntity>> MultipleAggregateRefsUsingDialog { get; set; } = new List<AggregateReference<SaveableEntity>>();
+    }
+}
