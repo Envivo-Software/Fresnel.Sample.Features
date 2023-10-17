c4efc2209b50e75b6f1d97d06bee5ff6d352ae49
vijay@envivo <vijay.patel@envivo.co.uk>
2023-10-17 21:36:41 +0100

    feat: Added examples of Relationships and UI behaviour

---
+++ Fresnel.Sample.Features.Model/A_Objects/RelationshipExamples/OwnedObject.cs
@@ -0,0 +1,23 @@
+// SPDX-FileCopyrightText: Copyright (c) 2022-2023 Envivo Software
+// SPDX-License-Identifier: Apache-2.0
+using Envivo.Fresnel.ModelAttributes;
+using Envivo.Fresnel.ModelTypes.Interfaces;
+using System;
+
+namespace Envivo.Fresnel.Sample.Features.Model.A_Objects.RelationshipExamples
+{
+    [Visible(isVisibleInLibrary: false)]
+    public class OwnedObject : IEntity
+    {
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        public Guid Id { get; set; }
+
+        public string Property1 { get; set; }
+
+        public int Property2 { get; set; }
+
+        public DateTime Property3 { get; set; }
+    }
+}
