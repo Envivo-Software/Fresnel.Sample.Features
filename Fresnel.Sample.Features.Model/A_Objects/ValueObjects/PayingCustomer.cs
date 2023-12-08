d7a72b881acc1cdf5ab50a23f2452b170d11bd2b
vijay@envivo <vijay.patel@envivo.co.uk>
2023-12-08 16:53:01 +0000

    feat: Added example of Payment Value Object, to demonstrate transient vs persistent behaviour

---
+++ Fresnel.Sample.Features.Model/A_Objects/ValueObjects/PayingCustomer.cs
@@ -0,0 +1,28 @@
+﻿using Envivo.Fresnel.ModelAttributes;
+using Envivo.Fresnel.ModelTypes.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Envivo.Fresnel.Sample.Features.Model.A_Objects.ValueObjects
+{
+    public class PayingCustomer : IAggregateRoot, IPersistable
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [ConcurrencyCheck]
+        public long Version { get; set; }
+
+        public string Name { get; set; }
+
+        [Relationship(RelationshipType.Owns)]
+        [UI(renderOption: UiRenderOption.InlineExpanded)]
+        public ICollection<Payment> Payments { get; set; } = new List<Payment>();
+
+        public override string ToString()
+        {
+            return Name ?? base.ToString();
+        }
+    }
+}
---
+++ Fresnel.Sample.Features.Model/A_Objects/ValueObjects/Payment.cs
@@ -0,0 +1,27 @@
+﻿using Envivo.Fresnel.ModelTypes.Interfaces;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Envivo.Fresnel.Sample.Features.Model.A_Objects.ValueObjects
+{
+/// <summary>
+/// This object is only editable when new, and becomes read-only once it's saved.
+/// </summary>
+public class Payment : IValueObject
+{
+    [Key]
+    public Guid Id { get; set; }
+
+    [DataType(DataType.Date)]
+    public DateTime PaymentDate { get; set; } = DateTime.Now;
+
+    public decimal Amount { get; set; } = new();
+
+    public string OtherNotes { get; set; }
+
+    public override string ToString()
+    {
+        return $"{PaymentDate:d} : {Amount:c}";
+    }
+}
+}
