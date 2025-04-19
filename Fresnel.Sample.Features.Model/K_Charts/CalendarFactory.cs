16d8fdb8691c874184480bdf6aaf38239eb00c5f
vijay@envivo <vijay.patel@envivo.co.uk>
2025-04-19 16:31:40 +0100

    feat: Added example of Calendar view

---
+++ Fresnel.Sample.Features.Model/K_Charts/CalendarFactory.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using Envivo.Fresnel.ModelTypes;
+using Envivo.Fresnel.ModelTypes.Interfaces;
+
+namespace Envivo.Fresnel.Sample.Features.Model.K_Charts
+{
+    public class CalendarFactory : IFactory<Calendar>
+    {
+        private readonly Random _Random = new();
+
+        public Calendar Create()
+        {
+            var daysInOneYear = Enumerable.Range(1, 365).ToArray();
+
+            var startOfYear = new DateTime(DateTime.Now.Year, 1, 1);
+
+            var calendarEntries =
+                daysInOneYear
+                .Select(d => new Calendar.CalendarEntry
+                {
+                    Id = Guid.NewGuid(),
+                    Title = $"Entry for Day {d}",
+                    EntryDate = startOfYear.AddDays(d).AddHours(9 + _Random.Next(8)),
+                    Duration = TimeSpan.FromHours(1),
+                })
+                .OrderBy(d => d.Id)
+                .Take(90)
+                .ToList();
+
+            return new Calendar
+            {
+                Id = Guid.NewGuid(),
+                Title = $"Calendar for {startOfYear.Year}",
+                Entries = calendarEntries
+            };
+        }
+    }
+}
